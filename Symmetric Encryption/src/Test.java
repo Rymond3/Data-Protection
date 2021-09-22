a2877e848ce7bcb0d14b8bbca644da2c4efd1664
Rymond3 <36478308+Rymond3@users.noreply.github.com>
2021-09-22 11:01:42 +0200

    Simmetric encryption

---
+++ Symmetric Encryption/src/Test.java
@@ -0,0 +1,28 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SymmetricCipher s = new SymmetricCipher();
+		byte[] input = new byte[] { (byte)49, (byte)50, (byte)51, (byte)52, (byte)53, (byte)54, 
+				(byte)55, (byte)56, (byte)57, (byte)48, (byte)49, (byte)50, (byte)51, (byte)52,
+				(byte)53, (byte)54 };
+		
+		byte[] key = new byte[] { (byte)49, (byte)50, (byte)51, (byte)52, (byte)53, (byte)54, 
+				(byte)55, (byte)56, (byte)57, (byte)48, (byte)49, (byte)50, (byte)51, (byte)52,
+				(byte)53, (byte)54 };
+		
+		try {
+			byte[] plaintext = s.decryptCBC(s.encryptCBC(input, key), key);
+			
+			for (int i = 0; i < plaintext.length; ++i)
+				System.out.print(plaintext[i] + " ");
+			System.out.println(" ");
+				
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
